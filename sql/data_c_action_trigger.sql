b2807134fbedefbbfc415d78f8948b19049f5bb5
Florian HENRY <florian.henry@open-concept.pro>
2021-07-28 16:21:32 +0200

    complete c_action_comm

---
+++ sql/data_c_action_trigger.sql
@@ -0,0 +1,2 @@
+INSERT INTO llx_c_action_trigger(elementtype, code, label, description) VALUES ('livre@bibliotheque','LIVRE_CREATE','creation d un livre','');
+INSERT INTO llx_c_action_trigger(elementtype, code, label, description) VALUES ('livre@bibliotheque','LIVRE_VALIDATE','validation dun livre d un livre','');
