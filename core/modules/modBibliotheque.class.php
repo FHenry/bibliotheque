4560b36ac9a6756f86c4ef809db9757d4deb6b75
Florian HENRY <florian.henry@open-concept.pro>
2023-05-25 16:39:57 +0200

    on going

--- core/modules/modBibliotheque.class.php
+++ core/modules/modBibliotheque.class.php
@@ -115,10 +115,10 @@ class modBibliotheque extends DolibarrModules
 			),
 			// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context to 'all'
 			'hooks' => array(
-				//   'data' => array(
-				//       'hookcontext1',
+				   'data' => array(
+					   'contactcard',
 				//       'hookcontext2',
-				//   ),
+				   ),
 				//   'entity' => '0',
 			),
 			// Set this to 1 if features of module are opened to external users
