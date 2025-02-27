596c5ea7c199b30ab78f5fd2a0a11b0b7c2cdb81
Florian HENRY <florian.henry@open-concept.pro>
2025-02-27 15:40:35 +0100

    version 20.0

---
+++ core/triggers/interface_99_modBibliotheque_BibliothequeTriggers.class.php
@@ -0,0 +1,110 @@
+<?php
+/* Copyright (C) 2025 Alice Adminson <myemail@mycompany.com>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    core/triggers/interface_99_modBibliotheque_BibliothequeTriggers.class.php
+ * \ingroup bibliotheque
+ * \brief   Example trigger.
+ *
+ * Put detailed description here.
+ *
+ * \remarks You can create other triggers by copying this one.
+ * - File name should be either:
+ *      - interface_99_modBibliotheque_MyTrigger.class.php
+ *      - interface_99_all_MyTrigger.class.php
+ * - The file must stay in core/triggers
+ * - The class name must be InterfaceMytrigger
+ */
+
+require_once DOL_DOCUMENT_ROOT.'/core/triggers/dolibarrtriggers.class.php';
+
+
+/**
+ *  Class of triggers for Bibliotheque module
+ */
+class InterfaceBibliothequeTriggers extends DolibarrTriggers
+{
+	/**
+	 * Constructor
+	 *
+	 * @param DoliDB $db Database handler
+	 */
+	public function __construct($db)
+	{
+		parent::__construct($db);
+		$this->family = "demo";
+		$this->description = "Bibliotheque triggers.";
+		$this->version = self::VERSIONS['dev'];
+		$this->picto = 'bibliotheque@bibliotheque';
+	}
+
+	/**
+	 * Function called when a Dolibarr business event is done.
+	 * All functions "runTrigger" are triggered if file
+	 * is inside directory core/triggers
+	 *
+	 * @param string 		$action 	Event action code
+	 * @param CommonObject 	$object 	Object
+	 * @param User 			$user 		Object user
+	 * @param Translate 	$langs 		Object langs
+	 * @param Conf 			$conf 		Object conf
+	 * @return int              		Return integer <0 if KO, 0 if no triggered ran, >0 if OK
+	 */
+	public function runTrigger($action, $object, User $user, Translate $langs, Conf $conf)
+	{
+		if (!isModEnabled('bibliotheque')) {
+			return 0; // If module is not enabled, we do nothing
+		}
+
+		// Put here code you want to execute when a Dolibarr business events occurs.
+		// Data and type of action are stored into $object and $action
+
+		// You can isolate code for each action in a separate method: this method should be named like the trigger in camelCase.
+		// For example : COMPANY_CREATE => public function companyCreate($action, $object, User $user, Translate $langs, Conf $conf)
+		$methodName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', strtolower($action)))));
+		$callback = array($this, $methodName);
+		if (is_callable($callback)) {
+			dol_syslog(
+				"Trigger '".$this->name."' for action '$action' launched by ".__FILE__.". id=".$object->id
+			);
+
+			return call_user_func($callback, $action, $object, $user, $langs, $conf);
+		}
+//		if ($action=='COMPANY_MODIFY') {
+//			//qsdsqdq
+//		}
+
+
+		return 0;
+	}
+
+	private function companyModify($action, $object, User $user, Translate $langs, Conf $conf){
+		if(empty($object->status)) {
+			dol_include_once('bibliotheque/class/emprunt.class.php');
+			$emprunt = new Emprunt($this->db);
+			$result = $emprunt->countBySoc($object->id, $object);
+			if ($result<0){
+				$this->errors=array_merge($this->errors,$emprunt->errors,[$emprunt->error]);
+				return $result;
+			} elseif($result>0) {
+				$this->errors[]=$langs->trans("YouCannotClosCustomerIfRentIsOnGoing",$result);
+				return -1;
+			}
+		}
+	}
+}
