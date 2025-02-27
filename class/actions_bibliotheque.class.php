92adbdfe8f823f7f025b014442dc98cdbf66dd41
Florian HENRY <florian.henry@open-concept.pro>
2025-02-27 16:34:06 +0100

    version 20.0

---
+++ class/actions_bibliotheque.class.php
@@ -0,0 +1,181 @@
+<?php
+/* Copyright (C) 2025 Alice Adminson <myemail@mycompany.com>
+ * Copyright (C) 2024		MDW							<mdeweerd@users.noreply.github.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    bibliotheque/class/actions_bibliotheque.class.php
+ * \ingroup bibliotheque
+ * \brief   Example hook overload.
+ *
+ * Put detailed description here.
+ */
+
+require_once DOL_DOCUMENT_ROOT.'/core/class/commonhookactions.class.php';
+
+/**
+ * Class ActionsBibliotheque
+ */
+class ActionsBibliotheque extends CommonHookActions
+{
+	/**
+	 * @var DoliDB Database handler.
+	 */
+	public $db;
+
+	/**
+	 * @var string Error code (or message)
+	 */
+	public $error = '';
+
+	/**
+	 * @var array Errors
+	 */
+	public $errors = array();
+
+
+	/**
+	 * @var array Hook results. Propagated to $hookmanager->resArray for later reuse
+	 */
+	public $results = array();
+
+	/**
+	 * @var ?string String displayed by executeHook() immediately after return
+	 */
+	public $resprints;
+
+	/**
+	 * @var int		Priority of hook (50 is used if value is not defined)
+	 */
+	public $priority;
+
+
+	/**
+	 * Constructor
+	 *
+	 *  @param		DoliDB		$db      Database handler
+	 */
+	public function __construct($db)
+	{
+		$this->db = $db;
+	}
+
+	/**
+	 * Overloading the tabContentViewInvoice function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             Return integer < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function tabContentViewInvoice($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+
+		/*var_dump($parameters);var_dump($object); echo "action: " . $action;
+		exit;*/
+
+		if (in_array($parameters['currentcontext'], array('invoicecard'))) {	    // do something only for the context 'somecontext1' or 'somecontext2'
+			// Do what you want here...
+			// You can for example load and use call global vars like $fieldstosearchall to overwrite them, or update database depending on $action and GETPOST values.
+			if(!empty($object->socid)) {
+				dol_include_once('bibliotheque/class/emprunt.class.php');
+				$emprunt = new Emprunt($this->db);
+				$result = $emprunt->countBySoc($object->socid, $object);
+				if ($result<0){
+					setEventMessages($this->error,$this->errors, 'errors');
+				} elseif($result>0) {
+					setEventMessages($this->error,$this->errors, 'errors');
+					 print '<script type="text/javascript">
+						 $(document).ready(function() {
+							$(".valuefield.fieldname_type").append("<span class=\"badgeneutral\">'.$result.'</span>");
+						 });
+					 </script>';
+				}
+			}
+
+
+			return 0;
+		}
+
+		if (!$error) {
+			//$this->results = array('myreturn' => 999);
+			//$this->resprints = 'A text to show';
+			return 0; // or return 1 to replace standard code
+		} else {
+			$this->errors[] = 'Error message';
+			return -1;
+		}
+	}
+
+	/**
+	 * Overloading the tabContentViewInvoice function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             Return integer < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager)
+	{
+		global $langs;
+
+		if (in_array($parameters['currentcontext'], array('invoicecard'))) {
+
+			print dolGetButtonAction($langs->trans('EnoterButton'), '', 'default', '#', '');
+		}
+	}
+
+
+	/**
+	 * Overloading the doActions function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             Return integer < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function doActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2'))) {	    // do something only for the context 'somecontext1' or 'somecontext2'
+			// Do what you want here...
+			// You can for example load and use call global vars like $fieldstosearchall to overwrite them, or update database depending on $action and GETPOST values.
+			//header('location:');
+		}
+
+		if (!$error) {
+			$this->results = array('myreturn' => 999);
+			$this->resprints = 'A text to show';
+			return 0; // or return 1 to replace standard code
+		} else {
+			$this->errors[] = 'Error message';
+			return -1;
+		}
+	}
+
+
+	/* Add here any other hooked methods... */
+}
--- core/modules/modBibliotheque.class.php
+++ core/modules/modBibliotheque.class.php
@@ -120,11 +120,10 @@ class modBibliotheque extends DolibarrModules
 			// Set here all hooks context managed by module. To find available hook context, make a "grep -r '>initHooks(' *" on source code. You can also set hook context to 'all'
 			/* BEGIN MODULEBUILDER HOOKSCONTEXTS */
 			'hooks' => array(
-				//   'data' => array(
-				//       'hookcontext1',
-				//       'hookcontext2',
-				//   ),
-				//   'entity' => '0',
+				   'data' => array(
+				       'invoicecard',
+				   ),
+				   //'entity' => '0',
 			),
 			/* END MODULEBUILDER HOOKSCONTEXTS */
 			// Set this to 1 if features of module are opened to external users
